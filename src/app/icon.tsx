ca2b8860929012659675cecadbad2519df9cba57
wafiamrullahazzaky <amrullahazzaky44@gmai.com>
2026-09-26 02:02:24 +0700

    Fix PWA Icons

---
+++ src/app/icon.tsx
@@ -0,0 +1,40 @@
+import { ImageResponse } from 'next/og'
+ 
+export const runtime = 'edge'
+export const size = { width: 512, height: 512 }
+export const contentType = 'image/png'
+ 
+export default function Icon() {
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          background: '#2563eb',
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          borderRadius: '128px',
+        }}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="256"
+          height="256"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="white"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        >
+          <path d="M21 12V7H5a2 2 0 0 1 0-4h14v4" />
+          <path d="M3 5v14a2 2 0 0 0 2 2h16v-5" />
+          <path d="M18 12a2 2 0 0 0 0 4h4v-4Z" />
+        </svg>
+      </div>
+    ),
+    { ...size }
+  )
+}
